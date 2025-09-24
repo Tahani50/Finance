1ff5ac51171c9e6ff8e8bd255d4ec2851f59daad
Tahani50 <tahanyayman4@gmail.com>
2025-09-24 11:33:45 +0300

    Create DatabaseManager

---
+++ Finance/Core/DatabaseManager.swift
@@ -0,0 +1,42 @@
+//
+//  DatabaseManager.swift
+//  Finance
+//
+//  Created by Tahani on 02/04/1447 AH.
+//
+
+import Foundation
+import GRDB
+
+final class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    let dbQueue: DatabaseQueue
+
+    private init() {
+        do {
+            let databaseURL = try FileManager.default
+                .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                .appendingPathComponent("finance.sqlite")
+            dbQueue = try DatabaseQueue(path: databaseURL.path)
+            try migrator.migrate(dbQueue)
+        } catch {
+            fatalError("Database init failed: \(error)")
+        }
+    }
+
+    private var migrator: DatabaseMigrator {
+        var migrator = DatabaseMigrator()
+
+        migrator.registerMigration("v1") { db in
+            try db.create(table: "user") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("name", .text)
+                    .notNull()
+                    .unique()
+            }
+        }
+
+        return migrator
+    }
+}
