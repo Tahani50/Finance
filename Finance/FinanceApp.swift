703fdbf759d9298bc8fe55a34d78e9c673258bb4
Tahani50 <tahanyayman4@gmail.com>
2025-09-24 09:12:11 +0300

    Initial Commit

---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by Tahani on 02/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
